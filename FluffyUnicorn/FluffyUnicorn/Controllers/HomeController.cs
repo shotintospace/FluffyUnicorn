eb89e5fb10bb04370868b991102ea703268fa86e
Mariusz <Mariusz@VISUALSTUDIOVM>
2016-08-26 02:09:28 +0000

    Add CPU loop

--- FluffyUnicorn/FluffyUnicorn/Controllers/HomeController.cs
+++ FluffyUnicorn/FluffyUnicorn/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace FluffyUnicorn.Controllers
     {
         public ActionResult Index()
         {
+            for (Int32 i = 0; i < 500; i++)
+            {
+                string test = "1";
+            }    
             return View();
         }
 
